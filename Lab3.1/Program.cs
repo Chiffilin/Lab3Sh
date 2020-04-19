00052ffd35c729f5dbbb5e6cb8b2308d6039dcfa
Chiffilin <dimaaitbaev42@gmail.com>
2020-04-20 02:45:06 +0300

    Добавьте файлы проекта.

---
+++ Lab3.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+public class Example
+{
+    public static void Main()
+    {
+        double R;
+        Console.WriteLine("Введите первое комплексное число : ");
+        Console.WriteLine("Введите действительную часть: ");
+        double a1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите мнимую часть: ");
+        double a2 = Convert.ToDouble(Console.ReadLine());
+        Complex a = new Complex(a1,a2);
+        Console.WriteLine(a);
+
+        Console.WriteLine("Введите второе комплексное число : ");
+        Console.WriteLine("Введите действительную часть: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите действительную часть: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        Complex b = new Complex(b1, b2);
+        Console.WriteLine(b);
+
+        Console.WriteLine("Введите третье комплексное число : ");
+        Console.WriteLine("Введите действительную часть: ");
+        double c1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите действительную часть: ");
+        double c2 = Convert.ToDouble(Console.ReadLine());
+        Complex c = new Complex(b1, b2);
+        Console.WriteLine(c);
+
+        Console.WriteLine("Введите четвертое комплексное число : ");
+        Console.WriteLine("Введите действительную часть: ");
+        double d1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите действительную часть: ");
+        double d2 = Convert.ToDouble(Console.ReadLine());
+        Complex d = new Complex(b1, b2);
+        Console.WriteLine(d);
+     
+    Console.WriteLine(a) ;
+    }
+    
+}
